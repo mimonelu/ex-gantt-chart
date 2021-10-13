61d2e7e3dccd344eb0930b6ebbaeaa4e05d93e87
mimonelu <mimonelu@gmail.com>
2021-10-13 17:00:34 +0900

    update

--- src/components/GanttChart.vue
+++ src/components/GanttChart.vue
@@ -1,5 +1,11 @@
 <template>
-  <table class="gantt-chart">
+  <table
+    class="gantt-chart"
+    :style="{
+      '--gantt-chart-v-main-separator-width': `calc(100% / ${hoursOfTerm / mainSeparatorSpan})`,
+      '--gantt-chart-v-sub-separator-width': `calc(100% / ${hoursOfTerm / subSeparatorSpan})`,
+    }"
+  >
     <tbody>
       <tr
         v-for="row, rowIndex in data"
@@ -7,18 +13,18 @@
       >
         <th
           v-for="header, headerIndex in row.headers"
-          :key="`header-${rowIndex}-${headerIndex}`"
+          :key="`body-header-${rowIndex}-${headerIndex}`"
           :rowspan="rowSpan(rowIndex, headerIndex)"
           :style="{ display: header.rowSpan ? 'none': '' }"
         >
-          <div class="header-content">{{ header.label }}</div>
+          <div class="body-header-content">{{ header.label }}</div>
         </th>
         <td>
           <div
             v-for="bar, barIndex in row.bars"
             :key="barId(rowIndex, barIndex)"
-            :id="barId(rowIndex, barIndex)"
             class="bar"
+            :data-id="barId(rowIndex, barIndex)"
             :data-visible="barVisible(rowIndex, barIndex).toString()"
           >
             <div class="bar-content">{{ bar.label }}</div>
@@ -38,29 +44,47 @@ export default {
       type: Array,
       required: true
     },
+
     from: {
       type: String,
       default () {
         const date = new Date()
+        date.setDate(date.getDate() - 1)
         date.setHours(0)
         date.setMinutes(0)
         date.setSeconds(0)
         return date.toLocaleString()
       }
     },
+
     to: {
       type: String,
       default () {
         const date = new Date()
+        date.setDate(date.getDate() + 1)
         date.setHours(23)
         date.setMinutes(59)
         date.setSeconds(59)
         return date.toLocaleString()
       }
+    },
+
+    mainSeparatorSpan: {
+      type: Number,
+      default: 6
+    },
+
+    subSeparatorSpan: {
+      type: Number,
+      default: 1
     }
   },
 
   computed: {
+    hoursOfTerm () {
+      return (this.toTime - this.fromTime) / 1000 / 60 / 60
+    },
+
     fromTime () {
       return new Date(this.from).getTime()
     },
@@ -112,7 +136,7 @@ export default {
         }
         const bar = this.data[rowIndex].bars[barIndex]
         const barId = this.barId(rowIndex, barIndex)
-        const $bar = this.$el.querySelector(`#${barId}`)
+        const $bar = this.$el.querySelector(`[data-id="${barId}"]`)
         const barFromTime = new Date(bar.from).getTime()
         const barToTime = new Date(bar.to).getTime()
         const left = (barFromTime - this.fromTime) / (this.toTime - this.fromTime) * 100
@@ -127,11 +151,25 @@ export default {
 <style>
 .gantt-chart {
   --gantt-chart-border-color: #c0c0c0;
-  --gantt-chart-header-bg-color: #e0e0e0;
-  --gantt-chart-header-fg-color: #202020;
+
+  /* ボディヘッダー */
+  --gantt-chart-body-header-bg-color: #e0e0e0;
+  --gantt-chart-body-header-fg-color: #202020;
+
+  /* ボディデータ */
+  --gantt-chart-body-data-bg-color: #f0f0f0;
+
+  /* 縦のメイン分割線 */
+  --gantt-chart-v-main-separator-color: #c0c0c0;
+  --gantt-chart-v-main-separator-width: calc(100% / 24); /* スクリプト制御 */
+
+  /* 縦のサブ分割線 */
+  --gantt-chart-v-sub-separator-color: #e0e0e0;
+  --gantt-chart-v-sub-separator-width: calc(100% / 48); /* スクリプト制御 */
+
+  /* バー */
   --gantt-chart-bar-bg-color: #0080f0;
   --gantt-chart-bar-fg-color: #f0f0f0;
-  --gantt-chart-content-bg-color: #f0f0f0;
 }
 </style>
 
@@ -143,34 +181,39 @@ export default {
 }
 
 th {
-  background-color: var(--gantt-chart-header-bg-color);
+  background-color: var(--gantt-chart-body-header-bg-color);
   padding: 0.5em 1em;
   user-select: none;
 }
 
-.header-content {
-  color: var(--gantt-chart-header-fg-color);
+.body-header-content {
+  color: var(--gantt-chart-body-header-fg-color);
   white-space: pre;
 }
 
 td {
-  background-color: var(--gantt-chart-content-bg-color);
+  background-color: var(--gantt-chart-body-data-bg-color);
   overflow-x: hidden;
   padding: 0;
   position: relative;
   width: 100%;
+
+  /* 縦の分割線 */
+  background-image:
+    repeating-linear-gradient(90deg, var(--gantt-chart-v-main-separator-color), var(--gantt-chart-v-main-separator-color) 1px, transparent 1px, transparent var(--gantt-chart-v-main-separator-width)),
+    repeating-linear-gradient(90deg, var(--gantt-chart-v-sub-separator-color), var(--gantt-chart-v-sub-separator-color) 1px, transparent 1px, transparent var(--gantt-chart-v-sub-separator-width));
+  background-position: -1px 0;
+  background-size: auto auto;
 }
 
 .bar {
   background-color: var(--gantt-chart-bar-bg-color);
   border-radius: 0.5em;
   box-sizing: border-box;
+  margin: 0.25em 0;
   overflow: hidden;
   padding: 0.5em 1em;
   position: relative;
-
-  /* バーのマージン代わり */
-  border: 2px solid var(--gantt-chart-content-bg-color);
 }
 .bar[data-visible="false"] {
   display: none;
